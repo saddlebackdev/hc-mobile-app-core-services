2f1acb5d836abe7f4371908f31abf86243273208
Bittu <bittu1028@gmail.com>
2021-10-20 20:18:58 +0545

    Added security service

---
+++ src/coreServices/authService.tsx
@@ -0,0 +1,79 @@
+import {Buffer} from 'buffer';
+import {authorize, revoke} from 'react-native-app-auth';
+// @ts-ignore
+import _ from 'lodash';
+import moment from 'moment';
+import {setItem, getItem, removeItem} from './storageService';
+
+export const login = async (
+  config: any,
+  storageType: string,
+  cb: () => string,
+) => {
+  try {
+    const authState = await authorize(config);
+    const jwtBody = authState.idToken.split('.')[1];
+    const base64 = jwtBody.replace('-', '+').replace('_', '/');
+    const decodedJwt = Buffer.from(base64, 'base64');
+    const idTokenJSON = JSON.parse(decodedJwt.toString());
+    await setItem('accessToken', authState.accessToken, storageType);
+    await setItem(
+      'accessTokenExpirationDate',
+      authState.accessTokenExpirationDate,
+      'asyncStorage',
+    );
+    await setItem('refreshToken', authState.refreshToken, storageType);
+    await setItem('uniqueId', idTokenJSON.sub, storageType);
+    cb();
+    return Promise.resolve();
+  } catch (error) {
+    // Failed to login
+    return Promise.reject();
+  }
+};
+
+export const isLoggedIn = async () => {
+  const accessToken = await getItem('accessToken', 'asyncStorage');
+  const accessTokenExpirationDate = await getItem(
+    'accessTokenExpirationDate',
+    'asyncStorage',
+  );
+
+  if (_.isNil(accessToken)) {
+    return false;
+  }
+
+  const isExpiredAccessToken = moment()
+    .utc()
+    .isAfter(accessTokenExpirationDate);
+
+  if (isExpiredAccessToken) {
+    return false;
+  }
+
+  return true;
+};
+
+export const logout = async (config: any, cb: () => string) => {
+  try {
+    let accessToken = await getItem('accessToken', 'asyncStorage');
+    await revoke(config, {
+      tokenToRevoke: accessToken!,
+      sendClientId: true,
+    });
+
+    await removeItem('accessToken', 'asyncStorage');
+    await removeItem('accessTokenExpirationDate', 'asyncStorage');
+    await removeItem('refreshToken', 'asyncStorage');
+    await removeItem('uniqueId', 'asyncStorage');
+    await removeItem('churchId', 'asyncStorage');
+    await removeItem('PersonQrCode', 'asyncStorage');
+
+    cb();
+    accessToken = '';
+    return Promise.resolve();
+  } catch (error) {
+    // Failed to logout
+    return Promise.reject();
+  }
+};
---
+++ src/coreServices/storageService.tsx
@@ -0,0 +1,42 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const setItem = async (key: string, value: string, type: string) => {
+  switch (type) {
+    case 'asyncStorage': {
+      await AsyncStorage.setItem(key, value);
+      break;
+    }
+    default: {
+      await AsyncStorage.setItem(key, value);
+    }
+  }
+  return true;
+};
+
+export const getItem = async (key: string, type: string) => {
+  let value = null;
+  switch (type) {
+    case 'asyncStorage': {
+      value = await AsyncStorage.getItem(key);
+      break;
+    }
+    default: {
+      value = await AsyncStorage.getItem(key);
+    }
+  }
+  return value;
+};
+
+export const removeItem = async (key: string, type: string) => {
+  let value = null;
+  switch (type) {
+    case 'asyncStorage': {
+      value = await AsyncStorage.removeItem(key);
+      break;
+    }
+    default: {
+      value = await AsyncStorage.removeItem(key);
+    }
+  }
+  return value;
+};
