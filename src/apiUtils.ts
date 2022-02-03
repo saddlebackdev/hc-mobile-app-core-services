71d8e7d3b4093a4ed1acf68f406affdb85a9248f
Ajay Gupta <ajay.gupta@bushel.co.in>
2022-02-03 16:17:28 +0530

    update api utils - complete

--- src/apiUtils.ts
+++ src/apiUtils.ts
@@ -1,41 +1,126 @@
-// @flow
-
 // Modules
 import Axios from 'axios';
 
-// Assets
+// Types
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import type { AxiosError, AxiosInstance, AxiosResponse } from 'axios';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import type { IExtendedAxiosInstance, IMiddleware } from './apiUtils.types';
 
-// Create Instance
+// Create API
+export const createApi = (baseURL: string): IExtendedAxiosInstance => {
+  // Create Axios Instance
+  const axiosInstance: AxiosInstance = Axios.create({ baseURL });
 
-export const loadApiUtils = (baseURL: string): any => {
-  const ApiUtills: any = Axios.create({
-    baseURL: baseURL,
-  });
+  // Middlewares
+  const requestSuccessMiddlewares: Array<IMiddleware> = [];
+  const requestFailureMiddlewares: Array<IMiddleware> = [];
+  const responseSuccessMiddlewares: Array<IMiddleware> = [];
+  const responseFailureMiddlewares: Array<IMiddleware> = [];
+
+  // Note: Need Axios 0.22+
+  // eslint-disable-next-line no-undef
+  const abortController = new AbortController();
 
   // Request Interceptor
-  ApiUtills.interceptors.request.use((config: any) => {
-    // Intercept outgoing requests here
+  axiosInstance.interceptors.request.use(
+    (request) => {
+      if (requestSuccessMiddlewares.length) {
+        requestSuccessMiddlewares.map((middleware) => {
+          request = middleware.run(request);
+        });
+      }
 
-    return config;
-  });
+      // @ts-ignore
+      if (request.cancel) {
+        request.signal = abortController.signal;
+        abortController.abort();
+      }
+
+      return request;
+    },
+    (exception) => {
+      if (requestFailureMiddlewares.length) {
+        requestFailureMiddlewares.map((middleware) => {
+          middleware.run(exception);
+        });
+      }
+
+      return Promise.reject(exception);
+    }
+  );
 
   // Response Interceptor
-  ApiUtills.interceptors.response.use(
-    (response: any) => {
-      // Intercept incoming responses here
+  axiosInstance.interceptors.response.use(
+    // Success
+    (response: AxiosResponse) => {
+      if (responseSuccessMiddlewares.length) {
+        responseSuccessMiddlewares.map((middleware) => {
+          middleware.run(response);
+        });
+      }
 
       return Promise.resolve(response);
     },
-    (error: any) => {
-      // Intercept incoming responses here
+    // Error
+    (exception: AxiosError) => {
+      if (exception.message === 'canceled') {
+        return Promise.reject(exception);
+      }
 
-      return Promise.reject(error);
+      if (responseFailureMiddlewares.length) {
+        responseFailureMiddlewares.map((middleware) => {
+          middleware.run(exception);
+        });
+      }
+
+      return Promise.reject(exception);
     }
   );
-  return ApiUtills;
+
+  // Use Request Middleware
+  const useRequestMiddleware = (middleware: IMiddleware): void => {
+    if (middleware?.on === 'success') {
+      requestSuccessMiddlewares.push(middleware);
+      return;
+    }
+
+    if (middleware?.on === 'failure') {
+      requestFailureMiddlewares.push(middleware);
+      return;
+    }
+
+    requestSuccessMiddlewares.push(middleware);
+    requestFailureMiddlewares.push(middleware);
+  };
+
+  // Use Response Middleware
+  const useResponseMiddleware = (middleware: IMiddleware): void => {
+    if (middleware?.on === 'success') {
+      responseSuccessMiddlewares.push(middleware);
+      return;
+    }
+
+    if (middleware?.on === 'failure') {
+      responseFailureMiddlewares.push(middleware);
+      return;
+    }
+
+    responseSuccessMiddlewares.push(middleware);
+    responseFailureMiddlewares.push(middleware);
+  };
+
+  // Extend Axios Instance
+  const extendedAxiosInstance: IExtendedAxiosInstance = Object.assign(
+    {
+      useRequestMiddleware,
+      useResponseMiddleware,
+    },
+    axiosInstance
+  );
+
+  return extendedAxiosInstance;
 };
 
 // Exports
-export default {
-  loadApiUtils,
-};
+export default { createApi };
