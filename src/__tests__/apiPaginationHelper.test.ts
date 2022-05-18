2277c554b17c6df50c001de8fa58b37955b9d25c
Oscar Mendoza <oscarResendizUaq@gmail.com>
2022-05-18 11:30:13 -0500

    pagination - unit testing

---
+++ src/__tests__/apiPaginationHelper.test.ts
@@ -0,0 +1,138 @@
+// npx jest --watch ./src/__tests__/apiPaginationHelper.test.ts
+import ApiPaginationHelper from '../apiPaginationHelper';
+import * as AuthService from '../authService';
+
+jest.mock('../authService.ts');
+
+const mockedGetUri = jest.fn();
+const mockedGet = jest.fn();
+
+jest.mock('axios', () => {
+  return {
+    create: jest.fn(() => ({
+      get: mockedGet,
+      interceptors: {
+        request: { use: jest.fn(), eject: jest.fn() },
+        response: { use: jest.fn(), eject: jest.fn() },
+      },
+      getUri: mockedGetUri,
+    })),
+  };
+});
+
+const BASE_URL = 'hc-test.saddleback.com';
+let apiPaginationHelperInstance: ApiPaginationHelper;
+const mockedAbortController = {
+  signal: 'test',
+  abort: jest.fn(),
+};
+
+describe('apiPaginationHelper', () => {
+  const OLD_ENV = process.env;
+
+  beforeEach(() => {
+    jest.resetModules(); // Most important - it clears the cache
+
+    process.env = {
+      ...OLD_ENV,
+      API_URL: BASE_URL,
+    }; // Make a copy
+
+    apiPaginationHelperInstance = new ApiPaginationHelper(
+      {
+        baseURL: BASE_URL,
+        url: '/api/some/endpoint',
+        params: {
+          oneParam: 1,
+          anotherParam: 'a',
+        },
+      },
+      mockedAbortController
+    );
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  afterAll(() => {
+    process.env = OLD_ENV; // Restore old environment
+  });
+
+  describe('Page number', () => {
+    it('should increment when calling each time we call getNextPage()', async () => {
+      expect(apiPaginationHelperInstance.getPage()).toBe(0);
+      await apiPaginationHelperInstance.getNextPage();
+      await apiPaginationHelperInstance.getNextPage();
+      await apiPaginationHelperInstance.getNextPage();
+      expect(apiPaginationHelperInstance.getPage()).toBe(3);
+    });
+
+    it('should start from 0 when getNextPage() is called with firstLoad', async () => {
+      expect(apiPaginationHelperInstance.getPage()).toBe(0);
+      await apiPaginationHelperInstance.getNextPage();
+      await apiPaginationHelperInstance.getNextPage();
+      expect(apiPaginationHelperInstance.getPage()).toBe(2);
+      await apiPaginationHelperInstance.getNextPage({}, true);
+      expect(apiPaginationHelperInstance.getPage()).toBe(1);
+    });
+
+    it('should reset to the inital value when calling resetPage() ', async () => {
+      expect(apiPaginationHelperInstance.getPage()).toBe(0);
+      await apiPaginationHelperInstance.getNextPage();
+      await apiPaginationHelperInstance.getNextPage();
+      await apiPaginationHelperInstance.getNextPage();
+      expect(apiPaginationHelperInstance.getPage()).toBe(3);
+      apiPaginationHelperInstance.resetPage();
+      expect(apiPaginationHelperInstance.getPage()).toBe(0);
+    });
+  });
+
+  describe('getUri()', () => {
+    it('must be called each time getNextPage() is called', async () => {
+      await apiPaginationHelperInstance.getNextPage();
+      expect(mockedGetUri).toHaveBeenCalledWith({
+        pageSize: 5,
+        page: 0,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        baseURL: 'hc-test.saddleback.com',
+        url: '/api/some/endpoint',
+        params: { oneParam: 1, anotherParam: 'a', page: 0 },
+      });
+      await apiPaginationHelperInstance.getNextPage();
+      await apiPaginationHelperInstance.getNextPage();
+      await apiPaginationHelperInstance.getNextPage();
+      expect(mockedGetUri).toHaveBeenCalledTimes(4);
+    });
+  });
+});
+
+describe('Access Token', () => {
+  it('is injected when the user is logged in and  the access token exist', async () => {
+    // @ts-ignore
+    AuthService.getAccessToken.mockImplementation(() => 'mocked_token');
+    // @ts-ignore
+    AuthService.isLoggedIn.mockImplementationOnce(() => true);
+    await apiPaginationHelperInstance.getNextPage();
+    let requestConfig = mockedGet.mock.calls[0][1];
+    let headers = requestConfig.headers;
+    expect(headers).toEqual({
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer mocked_token',
+    });
+    // @ts-ignore
+    AuthService.isLoggedIn.mockImplementationOnce(() => false);
+    await apiPaginationHelperInstance.getNextPage();
+    requestConfig = mockedGet.mock.calls[1][1];
+    expect(requestConfig).toEqual({
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+  });
+});
--- src/apiPaginationHelper.ts
+++ src/apiPaginationHelper.ts
@@ -1,102 +1,95 @@
-/* eslint-disable prettier/prettier */
-// import { axios } from './apiUtils';
-import { IExtendedAxiosInstance } from './apiUtils.types';
-import {
-    isLoggedIn,
-    getAccessToken,
-} from './authService';
-import type { AxiosRequestConfig } from 'axios';
+import { isLoggedIn, getAccessToken } from './authService';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import type { AxiosRequestConfig, AxiosInstance } from 'axios';
 import { createApi } from './apiUtils';
-import 'dotenv/config';
-
-// import 
-/**
- * TODOs:
- * config
- *  initialPage - 0 or 1 e.g. Optional?
- *  pageSize    - if exists, let's override the default
- *  baseUrl     - if exists, let's create a new axios instance
- * 
- * retrieve authorization token to inject it in the headers
- * 
- * Implement solutions for POST | PUT | DELETE
- * 
- * how will we handle params like search queries?
- */
+import isEmpty from 'lodash/isEmpty';
 
 const DEFAULT_CONFIG = {
-    pageSize: 5,
-    page: 0,
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-    },
+  pageSize: 5,
+  page: 0,
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  },
 };
 
 class ApiPaginationHelper {
-    page: number;
-    config: AxiosRequestConfig;
-    axios: IExtendedAxiosInstance;
+  page: number;
+  config: AxiosRequestConfig;
+  axios: AxiosInstance;
 
-    constructor(config: AxiosRequestConfig, mockedAbortController?: any) {
-        console.log('process.env.API_URL', process.env.API_URL);
-        this.page = 0;
-        this.config = config;
-        this.axios = createApi(
-            process.env.API_URL || config.baseURL || '',
-            {},
-            mockedAbortController,
-        );
+  constructor(config: AxiosRequestConfig, mockedAbortController?: any) {
+    this.page = 0;
+    this.config = config;
+    this.axios = createApi(
+      process.env.API_URL || config.baseURL || '',
+      {},
+      mockedAbortController
+    );
+  }
+
+  getPage() {
+    return this.page;
+  }
+
+  incrementPage() {
+    this.page = this.page + 1;
+  }
+
+  resetPage() {
+    this.page = 0;
+  }
+
+  setConfig(config: AxiosRequestConfig) {
+    this.config = config;
+  }
+
+  getUri() {
+    // Creates the URL and injects page and pageSize automatically
+    const uri = this.axios.getUri({
+      ...DEFAULT_CONFIG,
+      ...this.config,
+      params: {
+        ...this.config?.params,
+        page: this.page,
+      },
+    });
+
+    return uri;
+  }
+
+  async getNextPage(config?: AxiosRequestConfig, firstLoad?: boolean) {
+    if (!isEmpty(config)) {
+      this.setConfig({
+        ...config,
+      });
     }
 
-    incrementPage() {
-        this.page = this.page + 1;
+    if (firstLoad) {
+      this.resetPage();
     }
 
-    resetPage() {
-        this.page = 0;
+    const uri = this.getUri();
+
+    this.incrementPage();
+
+    const isUserLoggedIn = await isLoggedIn();
+
+    let authorizationToken;
+
+    if (isUserLoggedIn) {
+      authorizationToken = await getAccessToken();
     }
 
-    setConfig(config: AxiosRequestConfig) {
-        this.config = config;
-    }
-
-    async getNextPage(config?: AxiosRequestConfig, firstLoad?: boolean) {
-        if (config) {
-            this.setConfig(config);
-        }
-
-        if (firstLoad) {
-            this.resetPage();
-        }
-
-        // Creates the URL and injects page and pageSize automatically
-        const uri = this.axios.getUri({
-            ...DEFAULT_CONFIG,
-            ...this.config,
-            params: {
-                ...this.config?.params,
-                page: this.page,
-            },
-        });
-
-        this.incrementPage();
-
-        const isUserLoggedIn = await isLoggedIn();
-        let authorizationToken;
-
-        if (isUserLoggedIn) {
-            authorizationToken = await getAccessToken(); 
-        }
-
-        return this.axios.get(uri, {
-            ...(isUserLoggedIn && {
-                headers: {
-                    Authorization: `Bearer ${authorizationToken}`,
-                }
-            }),
-        });
-    }
+    return this.axios.get(uri, {
+      headers: {
+        ...DEFAULT_CONFIG.headers,
+        ...(isUserLoggedIn && {
+          Authorization: `Bearer ${authorizationToken}`,
+        }),
+      },
+    });
+  }
 }
 
 export default ApiPaginationHelper;
