728f5f676d48e9e34502f8b71af258d0bdecc1c1
Ajay Gupta <ajay.gupta@bushel.co.in>
2023-10-26 17:46:54 +0530

    refactor analytics service

--- src/analyticsService.ts
+++ src/analyticsService.ts
@@ -1,93 +1,147 @@
-import NavigationService from './navigationService';
+// Modules
+import { ApplicationInsights } from '@microsoft/applicationinsights-web';
+import { ReactNativePlugin } from '@microsoft/applicationinsights-react-native';
 
-// let instrumentationKey:string = '';
-let appInsights: any = null;
-let allowTracking: boolean = false;
-let commonTrackingProperties: object = {};
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import type {
+  IAppInsights,
+  IPageViewTelemetry,
+  IAutoExceptionTelemetry,
+  ICustomProperties,
+  IEventTelemetry,
+  IExceptionTelemetry,
+  IConfig,
+} from '@microsoft/applicationinsights-web';
 
-// export const setInstrumentationKey = (keyValue:string): void => {
-//     instrumentationKey = keyValue;
-// }
+// Interfaces
+interface IParams {
+  apiKey: string;
+  commonProps?: { [key: string | number]: any };
+  config?: IConfig;
+}
+export interface IAnalyticsInstance {
+  enable: () => void;
+  disable: () => void;
+  trackPageView: (pageName: string, props: any) => void;
+  trackException: (exception: any, props: any) => void;
+  trackEvent: (event: any, props: any) => void;
+  instance: IAppInsights;
+}
 
-export const setAndLoadAppInsights = (appInsightsConfig: any): void => {
-  appInsights = appInsightsConfig;
-  appInsights.loadAppInsights();
-};
+/**
+ * @method createAnalyticsInstance
+ * @description Creates an instance of the analytics service
+ * @param {object} options - The options object for the analytics service
+ * @param {string} options.apiKey - The API key for the analytics service
+ * @param {object} options.commonProps - Optional. The common properties to be sent with each event
+ * @param {object} options.config - Optional. The configuration object for the analytics service
+ */
+export const createAnalyticsInstance = function({
+  apiKey,
+  commonProps = {},
+  config = {},
+}: IParams): IAnalyticsInstance {
+  const RNPlugin = new ReactNativePlugin();
 
-// Get Route
-export const getRouteName = (page: string) => {
-  const route: any = NavigationService.getCurrentRoute();
-  return page || route!.name;
-};
+  // Flags
+  let isEnabled: boolean = true;
 
-export const setCommonTrackingPropertiesValue = (params: object) => {
-  commonTrackingProperties = params;
-};
+  /**
+   * @property instance
+   * @description The instance of AppInsights. It can use to access the unexposed methods and properties.
+   */
+  const instance: IAppInsights = new ApplicationInsights({
+    config: {
+      extensions: [RNPlugin],
+      instrumentationKey: apiKey,
+      ...config,
+    },
+  }).loadAppInsights();
 
-// Get Common Tracking Propreties
-export const getCommonTrackingProperties = (): object => {
-  return commonTrackingProperties;
-};
+  /**
+   * @method enable
+   * @description Enables analytics
+   */
+  const enable = (): void => {
+    isEnabled = true;
+  };
 
-export const setTrackingAllowed = (isAllowedTracking: boolean) => {
-  allowTracking = isAllowedTracking;
-};
+  /**
+   * @method disable
+   * @description Disables analytics
+   */
+  const disable = (): void => {
+    isEnabled = false;
+  };
 
-// Get Common Tracking Propreties
-export const verifyTrackingAllowed = (): boolean => {
-  return allowTracking;
-};
+  /**
+   * @method trackEvent
+   * @description Tracks an event
+   * @param event Name of the event
+   * @param customProperties Optional. Custom properties to be sent with the event
+   */
+  const trackEvent = (event: string, customProperties?: ICustomProperties): void => {
+    if (isEnabled) {
+      return;
+    }
 
-// Track Page View
-// Use this for Page Views or potentially modals and pop ups too??
-export const trackPage = (page: string, params: any, force?: boolean): void => {
-  const isTrackingAllowed = verifyTrackingAllowed();
-  if (isTrackingAllowed || force) {
-    const commonProps = getCommonTrackingProperties();
-    const pageName = getRouteName(page);
-    appInsights.trackPageView({
+    const customEvent: IEventTelemetry = {
+      name: event,
+    };
+
+    instance.trackEvent(customEvent, customProperties);
+  };
+
+  /**
+   * @method trackException
+   * @description Tracks an exception
+   * @param exception Name of the exception
+   * @param customProperties Optional. Custom properties to be sent with the exception
+   */
+  const trackException = (exception: IAutoExceptionTelemetry, customProperties?: ICustomProperties): void => {
+    if (isEnabled) {
+      return;
+    }
+
+    const exceptionEvent: Partial<IExceptionTelemetry> = {
+      exception,
+    };
+
+    instance.trackException(exceptionEvent, customProperties);
+  };
+
+  /**
+   * @method trackPageView
+   * @description Tracks a page view
+   * @param pageName Name of the page or route
+   * @param eventProperties Optional. Additional properties to be sent with the page view event
+   * @param customProperties Optional. Custom properties to be sent with the page view event
+   */
+  const trackPageView = (pageName: string, eventProperties: any, customProperties?: ICustomProperties) => {
+    if (!isEnabled) {
+      return;
+    }
+
+    const pageView: Partial<IPageViewTelemetry> = {
       name: pageName,
       properties: {
         ...commonProps,
-        ...params,
+        ...eventProperties,
       },
-    });
-  }
-};
+    };
 
-// Track Event
-// Use this for custom events i.e. API Errors, Searches, Links Out, etc.
-export const trackEvent = (type: string, params: any): void => {
-  const isTrackingAllowed = verifyTrackingAllowed();
+    instance.trackPageView(pageView, customProperties);
+  };
 
-  if (isTrackingAllowed) {
-    const commonProps = getCommonTrackingProperties();
-    const Data = params;
-    appInsights.trackEvent({
-      name: type,
-      properties: {
-        ...commonProps,
-        Data,
-      },
-    });
-  }
-};
-
-// Track Exceptions
-// Use this for exceptions
-export const trackException = (exception: any) => {
-  appInsights.trackException(exception);
+  return {
+    enable,
+    disable,
+    trackEvent,
+    trackException,
+    trackPageView,
+    instance,
+  };
 };
 
 // Exports
-export default {
-  getRouteName,
-  setAndLoadAppInsights,
-  setCommonTrackingPropertiesValue,
-  setTrackingAllowed,
-  trackEvent,
-  trackException,
-  verifyTrackingAllowed,
-  trackPage,
-  // setInstrumentationKey
-};
+export default { createAnalyticsInstance };
